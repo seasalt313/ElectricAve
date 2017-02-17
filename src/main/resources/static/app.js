a2280956c602c1ce46d15bab4f03c14ac921b41f
Mariam Qasim <mariam.a.qasim@gmail.com>
2017-02-17 13:01:55 -0500

    map still routes to ny

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -155,18 +155,18 @@ module.exports = {
     .bindPopup('Charlotte!')
     .openPopup();
 
-    ///////////////////////////
+    /////////////////////////
 
-        // tripService.showMap().then(function (response) {
-        //     L.geoJson(response.data).addTo(map);
-        //     console.log("starting coordinates");
-        //   //   start = response.data.features[0].geometry.coordinates[0];
-        //    //
-        //   //   console.log(start);
-        //    //
-        //    //
-        //   //  let loc = tripService.showLocation();
-        // });
+        tripService.showMap().then(function (response) {
+            L.geoJson(response.data).addTo(map);
+            console.log("starting coordinates");
+          //   start = response.data.features[0].geometry.coordinates[0];
+           //
+          //   console.log(start);
+           //
+           //
+          //  let loc = tripService.showLocation();
+        });
 
 
   }
