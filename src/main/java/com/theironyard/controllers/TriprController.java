2ea1e1be15c604721636fbeac916d8f7fb46182d
Ser-Castic <samsteinberg90@gmail.com>
2017-02-17 13:25:36 -0500

    Changed pathway name

--- src/main/java/com/theironyard/controllers/TriprController.java
+++ src/main/java/com/theironyard/controllers/TriprController.java
@@ -27,7 +27,7 @@ public class TriprController {
     UserRepository users;
 
 
-    @RequestMapping(path = "/map/{id}", method = RequestMethod.GET)
+    @RequestMapping(path = "/show-map/{id}", method = RequestMethod.GET)
     public GeoJSON directions(@RequestParam(value = "id") int id) throws Exception {
         GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBnADGOsZrhGtk1jSb8C9X49JoeG2m_KU0");
         DirectionsApiRequest directionsRequest = DirectionsApi.newRequest(context);
